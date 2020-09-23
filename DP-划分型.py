775a8a45e9181b7116a670b0c6679a1825ef92b4
MERCURYCOA <sncao62@hotmail.com>
2020-09-23 22:49:25 +0800

    Update DP-划分型.py

--- DP-划分型.py
+++ DP-划分型.py
@@ -1,3 +1,4 @@
+# f需要n+1个格子，初始化f[-1] =0, f里的i跟A里的i可以对齐，最后返回f[n-1],如果初始化f[0] = 1, f的i和A的i不对齐，即f[i]储存的是A[i-1]之前的总计数，最后返回f[n]
 class Solution:
     def decode_path(self, S):
         A = [int(x) for x in list(S)]
