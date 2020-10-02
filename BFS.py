63d342b5eaf71a757aae4cc94a088154cb0dce58
MERCURYCOA <sncao62@hotmail.com>
2020-10-02 13:44:59 +0800

    Update BFS.py

--- BFS.py
+++ BFS.py
@@ -59,3 +59,35 @@ class Solution:
                     level.append(node.right)
             queue = level
         return result
+# 题二： 跟题一倒过来，从最底层到root进行level order traversal
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+from collections import deque
+class Solution:
+    """
+    @param root: A tree
+    @return: buttom-up level order a list of lists of integer
+    """
+    def levelOrderBottom(self, root):
+        # write your code here
+        if not root:
+            return []
+            
+        result = []
+        queue = deque([root])
+        while queue:
+            level = []
+            level_size = len(queue)
+            for _ in range(level_size):
+                node = queue.popleft()
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            result.append(level)
+        return list(reversed(result))  #注意不要用list.reverse(),那样返回为空
+                level.append(node.val)
+                level.append(node.val)
