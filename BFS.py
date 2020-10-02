5b7ab4fe363f2f05010d3c9f8c46939a1898de10
MERCURYCOA <sncao62@hotmail.com>
2020-10-02 11:54:15 +0800

    Create BFS.py

---
+++ BFS.py
@@ -0,0 +1,35 @@
+# 题一：binary tree level order traversal
+# 用deque, popleft()时间复杂度O(1)
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+
+from collections import deque
+class Solution:
+    """
+    @param root: A Tree
+    @return: Level order a list of lists of integer
+    """
+    def levelOrder(self, root):
+        # write your code here
+        if not root:
+            return []
+            
+        result = []
+        queue = deque([root])
+        while queue:
+            level = []
+            level_size = len(queue)
+            for _ in range(level_size):
+                node = queue.popleft()
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            result.append(level)
+        return result
+   
+# 用list, insert, pop()时间复杂度 O(n)
