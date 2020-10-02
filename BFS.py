0fe28be512d8f708b520ec290b8048ee0e58a5a9
MERCURYCOA <sncao62@hotmail.com>
2020-10-02 12:15:07 +0800

    Update BFS.py

--- BFS.py
+++ BFS.py
@@ -32,4 +32,30 @@ class Solution:
             result.append(level)
         return result
    
-# 用list, insert, pop()时间复杂度 O(n)
+# 用list, pop()时间复杂度 O(n)
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+class Solution:
+    """
+    @param root: A Tree
+    @return: Level order a list of lists of integer
+    """
+    def levelOrder(self, root):
+        # write your code here
+        if not root:
+            return []
+            
+        result = []
+        queue = [root]
+        while queue:
+            level = []
+            result.append([node.val for node in queue])
+            for node in queue:
+                if node.left:
+                    level.append(node.left)
+                if node.right:
+                    level.append(node.right)
+            queue = level
+        return result
