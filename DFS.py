cefc69e9c84207688a6052249d21491d0f5a298a
MERCURYCOA <sncao62@hotmail.com>
2020-10-06 22:54:51 +0800

    Update DFS.py

--- DFS.py
+++ DFS.py
@@ -24,7 +24,8 @@ class Solution:
             if remainTarget < candidates[i]: # 当前值和之前current里得的数的和超过了target，比如：[2,2,2,2]，退出当前层的recursion函数，这时返回的是current是上一层的,比如：[2,2,2]
                 break
             current.append(candidates[i])  # 如果当前值与current里得的数的和没有超过target， 将当前值加入current
-            self.recursion(candidates, remainTarget - candidates[i], i, current, results)  # 下一层recursion的开始index还是i， 因为可以重复使用同一个数字
+            self.recursion(candidates, remainTarget - candidates[i], i, current, results)  # 下一层recursion的开始index还是i， 因为可以重复使用同一个数字； 
+            # 如果要求不能重复使用数字， i改成i+1
             current.pop() # 这里去掉current最后一个数，向后查看  比如[2,2,2,2]break之后， 返回的是[2,2,2]，这里去掉最后一个2， 在for循环中向后查看3
         
          
