7ce3c46a3b361058f3eaedb1ab58998094b67127
MERCURYCOA <sncao62@hotmail.com>
2020-10-09 19:42:12 +0800

    Update 链表和数组.py

--- 链表和数组.py
+++ 链表和数组.py
@@ -45,3 +45,52 @@ class Solution:
         head.next = nk
         n1.next = nkplus
         return n1
+
+# 题二： copy list with random pointer
+class RandomListNode:
+    def __init__(self, x):
+        self.label = x
+        self.next = None
+        self.random = None
+
+class Solution:
+    # @param head: A RandomListNode
+    # @return: A RandomListNode
+    def copyRandomList(self, head):
+        # write your code here
+        list1 = self.copyList(head)
+        list2 = self.randomPointer(list1)
+        return self.split(list2)
+    def copyList(self, head):
+        cur = head
+        while cur:
+            node = RandomListNode(cur.label)
+            node.next = cur.next
+            cur.next = node
+            cur = cur.next.next
+        return head
+    def randomPointer(self, head):
+        old = head
+        copy = head.next
+        while copy.next:
+            if old.random:
+                copy.random = old.random.next
+            else:
+                copy.random =  None
+            old = copy.next
+            copy = old.next
+        if copy:
+            if old.random:
+                copy.random = old.random.next
+            else:
+                copy.random =  None
+        return head
+    def split(self, head):
+        prev = head
+        res = cur = head.next
+        while cur.next:    
+            prev.next = prev.next.next
+            cur.next = cur.next.next
+            prev = prev.next
+            cur = cur.next
+        return res
