fa17e025dc2c76f67dbf6e2892d35778f3bc3ca5
MERCURYCOA <sncao62@hotmail.com>
2020-10-11 15:50:10 +0800

    Update 数组和双指针.py

--- 数组和双指针.py
+++ 数组和双指针.py
@@ -150,3 +150,36 @@ def deduplication(nums):
         return right + 1  # 这里要注意哪个指针代表uniqur数的结束
                 
        # print(nums)
+    
+ 
+
+
+#题二：构建data structure ,2个方法，可以加进去数，也可以返回是否存在一对数的和为value
+# 用dictionary最快
+class TwoSum:
+    """
+    @param number: An integer
+    @return: nothing
+    """
+    def __init__(self):     # 构建类， init一个map
+        self.map = {}
+    
+    def add(self, number):
+        # write your code here
+        if number in self.map:
+            self.map[number] += 1 
+            
+        else:
+            self.map[number] = 1 
+            
+            
+    """
+    @param value: An integer
+    @return: Find if there exists any pair of numbers which sum is equal to the value.
+    """
+    def find(self, value):
+        # write your code here
+        for num in self.map:
+            if value-num in self.map and (value-num != num or self.map[num]>1):
+                return True
+        return False 
