ca2c9f81991dceaec6c3a7e83bf2ffb425cd0af7
MERCURYCOA <sncao62@hotmail.com>
2020-10-12 12:57:33 +0800

    Update 数组和双指针.py

--- 数组和双指针.py
+++ 数组和双指针.py
@@ -306,4 +306,33 @@ class Solution:
                         left += 1 
         return res            
                     
-                    
+# 题七： 数组元素小于k在左边，大于等于k在右边
+# 快速选择
+class Solution:
+    """
+    @param nums: The integer array you should partition
+    @param k: An integer
+    @return: The index after partition
+    """
+    def partitionArray(self, nums, k):
+        # write your code here
+        if not nums:
+            return 0
+            
+        left, right = 0, len(nums)-1
+        
+        while left < right:
+            while left < right and nums[left]  < k:
+                left += 1 
+                
+            while left < right and nums[right] >= k:
+                right -= 1 
+                
+            if left < right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1 
+                right -= 1 
+        if nums[left] < k:  #这里的判断很关键，细节处理一定要想清楚分好类
+            return left+1
+            
+        return left
