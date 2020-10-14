ff4bc538b62971b364ad64969f5ee01c418484a5
MERCURYCOA <sncao62@hotmail.com>
2020-10-15 00:11:13 +0800

    Update 哈希表和堆.py

--- 哈希表和堆.py
+++ 哈希表和堆.py
@@ -146,3 +146,25 @@ class Solution:
                 break
             A[minIndex], A[index] = A[index], A[minIndex]
             index = minIndex
+# 堆的应用 heapq
+# 题二：ugly number II
+
+import heapq
+class Solution:
+    """
+    @param n: An integer
+    @return: return a  integer as description.
+    """
+    def nthUglyNumber(self, n):
+        # write your code here
+        heap = [1]
+        visited = set([1])
+        for i in range(n):
+            val = heapq.heappop(heap)
+            for factor in [2,3,5]:
+                if val*factor not in visited:
+                    visited.add(val*factor)
+                    heapq.heappush(heap, val*factor)
+                    
+        return val
+        
