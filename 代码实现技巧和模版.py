c895c90c415ff52adaf2aeb85d3580998d3fe826
MERCURYCOA <sncao62@hotmail.com>
2020-10-19 16:14:26 +0800

    Update 代码实现技巧和模版.py

--- 代码实现技巧和模版.py
+++ 代码实现技巧和模版.py
@@ -1,1 +1,10 @@
-# 双指针， j的循环移动，要把条件写到while后面
+# 模版一：窗口类指针， j的循环移动，要把条件写到while后面
+# 本质是对两层for循环的改进
+for i in range(n):
+  while j < n and i < j and j前进的条件：
+    j += 1
+    更新j状态
+   
+  if j停下的条件：
+    return/ break 
+  更新i的状态
