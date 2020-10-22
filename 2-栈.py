5e9ba4d98cfd74ed50dda8ef284341ee87e2988a
MERCURYCOA <sncao62@hotmail.com>
2020-10-22 14:12:04 +0800

    Update 2-栈.py

--- 2-栈.py
+++ 2-栈.py
@@ -68,3 +68,57 @@ class MyQueue:
         while self.stack_:
             self.stack.append(self.stack_.pop())
         return element
+    
+ # 题三： expression expand 字符串解码
+
+class Solution:
+    """
+    @param s: an expression includes numbers, letters and brackets
+    @return: a string
+    """
+    def expressionExpand(self, s):
+        if not s:
+            return ''
+        stack = []
+        
+        for c in s:
+            if c == ']':
+                strs = []
+                while stack and stack[-1] != '[':
+                    strs.append(stack.pop())
+                stack.pop() #弹出'['
+                num = 0
+                base = 1
+                while stack and stack[-1].isdigit():  # 出栈的数字顺序是反的，需要正过来
+                    num += int(stack.pop()) * base
+                    base *= 10
+                stack.append(''.join(reversed(strs))*num)  # strs是字符串数组，需要用''.join变成string
+           
+            else:
+                stack.append(c)
+        return ''.join(stack)        
+# 注意：这种解法不可以让'['直接忽略，还是需要压入栈内，然后到'['时pop出来。如果直接elif c =='[': continue, 3[2[ab]]这种情况会出现错误
+# 下面这种解法可以不让'['入栈，但是与上面的解法有2点不同 1: 数字在存入栈之前就转换好了,记为number 2：elif c=='[':stack.append(number), 遇到[时，就把相邻的number压入栈内
+class Solution:
+    # @param {string} s  an expression includes numbers, letters and brackets
+    # @return {string} a string
+    def expressionExpand(self, s):
+        stack = []
+        number = 0
+        for char in s:
+            if char.isdigit():
+                number = number * 10 + ord(char) - ord('0')
+            elif char == '[':
+                stack.append(number)
+                number = 0
+            elif char == ']':
+                strs = []
+                while len(stack):
+                    top = stack.pop()
+                    if type(top) == int:
+                        stack.append(''.join(reversed(strs)) * top)
+                        break
+                    strs.append(top)
+            else:
+                stack.append(char)
+        return ''.join(stack)
