46a147055215b9f0222586a6bb81032f47a67fd6
MERCURYCOA <sncao62@hotmail.com>
2020-11-24 22:32:33 +0800

    Update DP-划分型.py

--- DP-划分型.py
+++ DP-划分型.py
@@ -17,31 +17,24 @@ class Solution:
 
         #数组初值均为0，dp[0] = dp[1] = 1
 
-        dp = [1, 1]
-
-        for i in range(2,len(s) + 1):   # '192011'  dp = [1, 1, 2, 0]
-
-            dp.append(0)
-
-            #若s[i - 2]和s[i - 1]表示的数是10到26
-
-            if 10 <= int(s[i - 2 : i]) <= 26:
-
-                dp[i] += dp[i - 2]
-
+        n = len(s)
+        dp = [0]*(n+1)
+        dp[0] = 1
+        for i in range(1, n + 1):   # '192011'  dp = [1, 1, 2, 0]
             #若s[i - 1]表示的数是1到9
 
             if int(s[i - 1 : i]) != 0:
 
                 dp[i] += dp[i - 1]
 
-            #若上述两种情况都不满足，直接返回答案0
+            #若s[i - 2]和s[i - 1]表示的数是10到26
 
-            if dp[i] == 0:
+            if i > 1 and 10 <= int(s[i - 2 : i]) <= 26:  # i > 1重要
 
-                return 0
+                dp[i] += dp[i - 2]
 
-        return dp[len(s)]
+        return dp[n]
+
 
 # f需要n+1个格子，初始化f[-1] =0, f里的i跟A里的i可以对齐，最后返回f[n-1],如果初始化f[0] = 1, f的i和A的i不对齐，即f[i]储存的是A[i-1]之前的总计数，最后返回f[n]
 class Solution:
