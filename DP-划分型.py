134bea7edff2323cd60c5ccc8778e2305224cea0
MERCURYCOA <sncao62@hotmail.com>
2020-12-07 23:02:11 +0800

    Update DP-划分型.py

--- DP-划分型.py
+++ DP-划分型.py
@@ -55,3 +55,20 @@ class Solution:
 if __name__ == '__main__':
     solution = Solution()
     print(solution.decode_path("12"))
+# 题二：完美平方
+
+class Solution:
+    """
+    @param n: a positive integer
+    @return: An integer
+    """
+    def numSquares(self, n):
+        dp = [n for _ in range(n+1)]
+        for i in range(n+1):
+            if i**2 <= n:
+                dp[i**2] = 1
+            j = 1
+            while j**2 < i:
+                dp[i] = min(dp[i], dp[i-j**2] + 1)
+                j +=1
+        return dp[n]
