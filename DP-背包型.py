7a351313a61200f90f362ca85ad2a31059ded7b3
MERCURYCOA <sncao62@hotmail.com>
2020-12-19 21:36:14 +0800

    Update DP-背包型.py

--- DP-背包型.py
+++ DP-背包型.py
@@ -31,3 +31,50 @@ class Solution:
         return res
       
  
+# 题2 背包问题 V
+# 解法1: 2维滚动数组
+class Solution:
+    """
+    @param nums: an integer array and all positive numbers
+    @param target: An integer
+    @return: An integer
+    """
+    def backPackV(self, nums, target):
+        n = len(nums)
+        if n == 0:
+            return 0 
+            
+        dp = [[0 for _ in range(target+1)] for _ in range(2)]
+        dp[0][0] = 1 
+        
+            
+        for i in range(1, n+1):
+            for j in range(target+1):
+                dp[i%2][j] = dp[(i-1)%2][j]
+                if j >= nums[i-1]:
+                    dp[i%2][j] = dp[i%2][j] + dp[(i-1)%2][j - nums[i-1]]
+                    
+        return dp[n%2][target]
+    
+# 解法2: 1维滚动数组 + 从后向前 【因为需要用到前面的旧值，如果从前向后更新，新值会覆盖需要用到的旧值】
+class Solution:
+    """
+    @param nums: an integer array and all positive numbers
+    @param target: An integer
+    @return: An integer
+    """
+    def backPackV(self, nums, target):
+        n = len(nums)
+        if n == 0:
+            return 0 
+            
+        dp = [0 for _ in range(target+1)]
+        dp[0] = 1 
+        
+            
+        for i in range(1, n+1):
+            for j in range(target, -1, -1):
+                if j >= nums[i-1]:
+                    dp[j] += dp[j - nums[i-1]]
+                    
+        return dp[target]
