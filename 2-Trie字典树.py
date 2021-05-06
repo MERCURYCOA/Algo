a68a2e46d4c0e586a4c18fdbd9d527344017e840
MERCURYCOA <sncao62@hotmail.com>
2021-05-06 13:36:12 +0800

    Update 2-Trie字典树.py

--- 2-Trie字典树.py
+++ 2-Trie字典树.py
@@ -75,5 +75,100 @@ class WordDictionary:
         if not word:
             return False
         return self.find(self.root, word, 0)
+    
+# 题三： 查找树服务
+
+"""
+Definition of TrieNode:
+class TrieNode:
+    def __init__(self):
+        # <key, value>: <Character, TrieNode>
+        self.children = collections.OrderedDict()
+        self.top10 = []
+"""
+class TrieService:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def get_root(self):
+        # Return root of trie root, and 
+        # lintcode will print the tree struct.
+        return self.root
+
+    # @param {str} word a string
+    # @param {int} frequency an integer
+    # @return nothing
+    def insert(self, word, frequency):
+        # Write your your code here
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                node.children[c] = TrieNode()
+            node = node.children[c]
+            node.top10.append(frequency)
+            node.top10.sort(reverse=True) # 也可以不每次都sort，因为之前的已经都是sorted过的，就从后向前比较，交换，类似插入排序那样，插到对的位置，
+            
+            if len(node.top10) > 10:
+                node.top10.pop()
+        
+         
+  # 题四： 字典树 序列化与反序列化
+"""
+Definition of TrieNode:
+class TrieNode:
+    def __init__(self):
+        # <key, value>: <Character, TrieNode>
+        self.children = collections.OrderedDict()
+"""
+
+
+class Solution:
+
+    '''
+    @param root: An object of TrieNode, denote the root of the trie.
+    This method will be invoked first, you should design your own algorithm 
+    to serialize a trie which denote by a root node to a string which
+    can be easily deserialized by your own "deserialize" method later.
+    '''
+    def serialize(self, root):
+        # Write your code here
+        if root is None:
+            return ""
+        data = ""
+        
+        for k, v in root.children.items():  # 递归
+            data += k + self.serialize(v)
+        
+        return '<%s>' % data    # 注意记住这种写法，不需要对每个char都补上‘<''>'
+            
+
+
+    '''
+    @param data: A string serialized by your serialize method.
+    This method will be invoked second, the argument data is what exactly
+    you serialized at method "serialize", that means the data is not given by
+    system, it's given by your own serialize method. So the format of data is
+    designed by yourself, and deserialize it here as you serialize it in 
+    "serialize" method.
+    '''
+    def deserialize(self, data):  # 树的层级便利（list, pop ）
+        # Write your code here
+
+        if data is None or len(data) == 0:
+            return None 
+        root = TrieNode()
+        current = root
+        path = []
+
+        for c in data:
+            if c == '<':
+                path.append(current)
+            elif c == '>':
+                path.pop()
+            else:
+                path[-1].children[c] = TrieNode()
+                current = path[-1].children[c]
+        return root
         
   
